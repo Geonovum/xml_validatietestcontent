72f0c4283d7d88a6eabc67156afb7eb9654e48d3
BertVerhees <bert.verhees@rosa.nl>
2021-01-11 16:21:07 +0100

    GEO0001

---
+++ 1.0.4-rc/opdracht_voorbeeldbestanden/PerStuk/repair/repair_all.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/sh
+
+# Test alle tekst_nummer directories
+
+cd ..
+for d in *; do
+    if [ -d "$d" ]; then
+	  repair/repair_one.sh $d;
+	fi
+done
---
+++ 1.0.4-rc/opdracht_voorbeeldbestanden/PerStuk/repair/repair_one.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/sh
+
+
+#cd ../LVBB3507;zip a ../opdracht_lvbb3507.zip *;git add * ../*;git commit -a -m "lvbb3507";git push
+echo $1
+cd $1;
+cd ../$1;zip a ../opdracht_$1.zip *;git add * ../*;git commit -a -m $1;git push
+pwd;
+cd -;
