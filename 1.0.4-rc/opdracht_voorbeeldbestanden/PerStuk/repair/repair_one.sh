bbf623ef1e296f907a5b263fff0b8440a879f38d
BertVerhees <bert.verhees@rosa.nl>
2021-01-11 17:14:12 +0100

    GEO0001

--- 1.0.4-rc/opdracht_voorbeeldbestanden/PerStuk/repair/repair_one.sh
+++ 1.0.4-rc/opdracht_voorbeeldbestanden/PerStuk/repair/repair_one.sh
@@ -3,4 +3,4 @@
 
 #cd ../LVBB3507;zip a ../opdracht_lvbb3507.zip *;git add * ../*;git commit -a -m "lvbb3507";git push
 echo $1
-cd $1;zip a ../opdracht_$1.zip *;git add * opdracht_$1.zip;git commit -a -m $1;git push;cd ..;
+cd $1;zip a ../opdracht_$1.zip *;git add * ../opdracht_$1.zip;git commit -a -m $1;git push;cd ..;
