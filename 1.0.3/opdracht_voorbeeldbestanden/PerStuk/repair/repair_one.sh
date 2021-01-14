eb4c277e875319cb451bf9b77d315aaaacb0bda0
BertVerhees <bert.verhees@rosa.nl>
2021-01-14 14:50:42 +0100

    LVBB4005

--- 1.0.3/opdracht_voorbeeldbestanden/PerStuk/repair/repair_one.sh
+++ 1.0.3/opdracht_voorbeeldbestanden/PerStuk/repair/repair_one.sh
@@ -2,4 +2,4 @@
 
 
 echo $1
-cd $1;rm ../opdracht_$1.zip;zip a ../opdracht_$1.zip *;git add *;git add ../opdracht_$1.zip;git commit -a -m $1;git push;cd ..;
+cd $1;rm ../opdrachten_gereed/opdracht_$1.zip;zip a ../opdrachten_gereed/opdracht_$1.zip *;git add *;git add ../opdrachten_gereed/opdracht_$1.zip;git commit -a -m $1;git push;cd ..;
