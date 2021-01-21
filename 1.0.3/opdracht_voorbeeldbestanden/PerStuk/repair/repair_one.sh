1a3888d8de62d463f6e6b392f0b896d2486296be
BertVerhees <bert.verhees@rosa.nl>
2021-01-21 13:38:51 +0100

    OZON0017

--- 1.0.3/opdracht_voorbeeldbestanden/PerStuk/repair/repair_one.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/sh
-
-
-echo $1
-cd $1;rm ../../opdrachten_gereed/opdracht_$1.zip;zip a ../../opdrachten_gereed/opdracht_$1.zip *;git add *;git add ../../opdrachten_gereed/opdracht_$1.zip;git commit -a -m $1;git push;cd ..;
