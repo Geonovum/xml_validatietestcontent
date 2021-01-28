acd64f9c4e8d73a62c08da52b170c7529ad0517e
BertVerhees <bert.verhees@rosa.nl>
2021-01-28 11:02:32 +0100

    LVBB4017

--- 1.0.3/opdracht_voorbeeldbestanden/LVBB_/repair/repair_one.sh
+++ 1.0.3/opdracht_voorbeeldbestanden/LVBB_/repair/repair_one.sh
@@ -12,8 +12,8 @@ cd $1;
 RANDOM=tr -dc '[:alnum:]' < /dev/urandom  | dd bs=4 count=8 2>/dev/null
 NEWLINE="<idLevering>id-publicatie-$1-$RANDOM</idLevering>";
 echo $NEWLINE;
-echo "$RANDOM";
-echo "$RANDOM";
+echo $1-$RANDOM;
+echo $1-$RANDOM;
 sed -i "s|.*idLevering.*|$NEWLINE|" opdracht.xml
 rm ../../opdrachten_gereed/opdracht_$1*.zip;
 zip a ../../opdrachten_gereed/opdracht_$1_$RANDOM.zip *;
