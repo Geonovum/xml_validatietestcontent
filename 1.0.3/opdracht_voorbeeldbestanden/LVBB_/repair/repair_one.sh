c78b646fd86fe10a29ce9916de8c499a83ab524e
BertVerhees <bert.verhees@rosa.nl>
2021-01-28 10:47:01 +0100

    LVBB4017

--- 1.0.3/opdracht_voorbeeldbestanden/LVBB_/repair/repair_one.sh
+++ 1.0.3/opdracht_voorbeeldbestanden/LVBB_/repair/repair_one.sh
@@ -3,13 +3,13 @@
 
 echo $1
 cd $1;
-RANDOM=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1);
+RANDOM=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
 NEWLINE="<idLevering>id-publicatie-$1-$RANDOM</idLevering>";
 echo $NEWLINE;
 echo "$RANDOM";
 echo "$RANDOM";
 sed -i "s|.*idLevering.*|$NEWLINE|" opdracht.xml
-rm ../../opdrachten_gereed/opdracht_$1_*.zip;
+rm ../../opdrachten_gereed/opdracht_$1*.zip;
 zip a ../../opdrachten_gereed/opdracht_$1_$RANDOM.zip *;
 git add *;
 git add ../../opdrachten_gereed/opdracht_$1_$RANDOM.zip;
