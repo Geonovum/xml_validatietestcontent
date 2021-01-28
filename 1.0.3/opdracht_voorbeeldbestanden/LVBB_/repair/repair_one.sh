ae11442e8e9871ef8c3eafcf1c06078e2a5a7c10
BertVerhees <bert.verhees@rosa.nl>
2021-01-28 11:06:44 +0100

    LVBB4017

--- 1.0.3/opdracht_voorbeeldbestanden/LVBB_/repair/repair_one.sh
+++ 1.0.3/opdracht_voorbeeldbestanden/LVBB_/repair/repair_one.sh
@@ -10,16 +10,16 @@ tr -dc '[:alnum:]' < /dev/urandom  | dd bs=4 count=8 2>/dev/null
 echo $1
 cd $1;
 RANDOM=tr -dc '[:alnum:]' < /dev/urandom  | dd bs=4 count=8 2>/dev/null
-NEWLINE="<idLevering>id-publicatie-$1-$RANDOM</idLevering>";
+FILENAMEPART=$1_$RANDOM
+NEWLINE="<idLevering>id-publicatie-$FILENAMEPART</idLevering>";
 echo $NEWLINE;
 echo $NEWLINE;
-echo $1-$RANDOM;
-echo $1-$RANDOM;
-sed -i "s|.*idLevering.*|$NEWLINE|" opdracht.xml
+echo $FILENAMEPART;
+echo $FILENAMEPART;
+so d -i "s|.*idLevering.*|$NEWLINE|" opdracht.xml
 rm ../../opdrachten_gereed/opdracht_$1*.zip;
-zip a ../../opdrachten_gereed/opdracht_$1_$RANDOM.zip *;
+zip a ../../opdrachten_gereed/opdracht_$FILENAMEPART.zip *;
 git add *;
-git add ../../opdrachten_gereed/opdracht_$1_$RANDOM.zip;
+git add ../../opdrachten_gereed/opdracht_$FILENAMEPART.zip;
 git commit -a -m $1;git push;
 cd ..;
-
