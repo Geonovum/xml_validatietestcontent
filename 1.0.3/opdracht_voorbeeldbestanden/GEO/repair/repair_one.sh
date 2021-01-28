87c368b0813ebff5864af90ecc541d7664471114
BertVerhees <bert.verhees@rosa.nl>
2021-01-28 11:37:33 +0100

    GEO0001

--- 1.0.3/opdracht_voorbeeldbestanden/GEO/repair/repair_one.sh
+++ 1.0.3/opdracht_voorbeeldbestanden/GEO/repair/repair_one.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/sh
 
-
 echo $1
-cd $1;rm ../../opdrachten_gereed/opdracht_$1.zip;zip a ../../opdrachten_gereed/opdracht_$1.zip *;git add *;git add ../../opdrachten_gereed/opdracht_$1.zip;git commit -a -m $1;git push;cd ..;
+cd $1;
+RANDOM=tr -c -d '[:alnum:]' < /dev/urandom  | dd bs=4 count=8 2>/dev/null
+FILENAMEPART=$1_$RANDOM
+NEWLINE="<idLevering>id-publicatie-$FILENAMEPART</idLevering>";
+sed -i "s|.*idLevering.*|$NEWLINE|" opdracht.xml
+rm ../../opdrachten_gereed/opdracht_$1*.zip;
+zip a ../../opdrachten_gereed/opdracht_$FILENAMEPART.zip *;
+git add *;
+git add ../../opdrachten_gereed/opdracht_$FILENAMEPART.zip;
+git commit -a -m $1;git push;
+cd ..;
