6479577e5ea7c1141bd906193f0868964bf88fd3
BertVerhees <bert.verhees@rosa.nl>
2021-01-28 10:09:53 +0100

    LVBB4017

--- 1.0.3/opdracht_voorbeeldbestanden/LVBB_/repair/repair_one.sh
+++ 1.0.3/opdracht_voorbeeldbestanden/LVBB_/repair/repair_one.sh
@@ -2,4 +2,13 @@
 
 
 echo $1
-cd $1;rm ../../opdrachten_gereed/opdracht_$1.zip;zip a ../../opdrachten_gereed/opdracht_$1.zip *;git add *;git add ../../opdrachten_gereed/opdracht_$1.zip;git commit -a -m $1;git push;cd ..;
+cd $1;
+RANDOM=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1`
+# sed -i '/.*idLevering.*/d' opdracht.xml
+sed -i 's/.*idLevering.*/<idLevering>id-publicatie-$1-$RANDOM</idLevering>/' opdracht.xml
+rm ../../opdrachten_gereed/opdracht_$1.zip;
+zip a ../../opdrachten_gereed/opdracht_$1.zip *;
+git add *;
+git add ../../opdrachten_gereed/opdracht_$1.zip;
+git commit -a -m $1;git push;
+cd ..;
