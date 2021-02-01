539aee65b66d3641d3cf2ad2e30166a169b42437
BertVerheesGeonovum <b.verhees@geonovum.nl>
2021-02-01 16:24:36 +0000

    proceeding

--- 1.0.3/execute_tests/execute_test.sh
+++ 1.0.3/execute_tests/execute_test.sh
@@ -1,27 +1,80 @@
 #!/usr/bin/env bash
 
 
-ZIPFILE_DIR="../opdracht_voorbeeldbestanden/opdrachten_gereed";
+FILE_DIR="../opdracht_voorbeeldbestanden";
 FINDNUMBER=$1
 DEBUG=$2
 LOGLEVEL=""
+dt=`date '+%d%m%Y%H%M%S'`
+resultfile="../results/result$dt.xml"
+
+creating_zipfile_with_unique_levering_id () {
+	directory=$1
+        directoryname=$(basename $directory)
+	log_level=""
+	echo "Processing $directory $directoryname"
+
+	#building sed-command
+	RANDOM=tr -c -d '[:alnum:]' < /dev/urandom  | dd bs=4 count=8 2>/dev/null
+	FILENAMEPART="$directoryname-$RANDOM"
+	NEWLINE="<idLevering>id-publicatie-$FILENAMEPART</idLevering>";
+
+	#changing opdracht.xml and creating zipfile
+	cp $directory/opdracht.xml opdracht.bak;
+	sed -i "s|.*idLevering.*|$NEWLINE|" $directory/opdracht.xml
+	zip "opdracht_$FILENAMEPART.zip" $directory/*;
+	mv opdracht.bak $directory/opdracht.xml;
+
+	#execute keten-test
+	result=$(oow-corv $log_level --action versturen --levering_id "id-publicatie-$FILENAMEPART" --conversation_id "$FILENAMEPART" --oin 00000001812579446000 --opdracht valideren "opdracht_$FILENAMEPART.zip")
+	rm "opdracht_$FILENAMEPART.zip";
+
+	#wait 10 seconds for keten to create results
+	sleep 10
+	#get result
+	wget -nv --no-check-certificate $result -O result;
+	cat result>>$resultfile; 
+	rm result
+}
+
+execute_single_directory () {
+	directory=$1
+	directory_name=$(basename $directory)
+	creating_zipfile_with_unique_levering_id $directory
+}
 
 
-echo "<result>" > result.xml
+find_directory () {
+	DIRECTORIES=$(find $FILE_DIR/$1/* -name "*" -print);
+        for directory in $DIRECTORIES; do
+                if [ -d "$directory" ]; then
+                        execute_single_directory $directory
+                fi
+        done
+}
+
+
+echo "<result>">$resultfile
 if [[ -n $FINDNUMBER ]]; then
 	echo "Processing with argument $FINDNUMBER";
-	#find file and test if is a directory
-	NUMFILE=$(find $ZIPFILE_DIR -name "$FINDNUMBER*"|wc -l)
+	
+	#find directory and test if is a directory or wildcard
+	NUMFILE=$(find $FILE_DIR -name "$FINDNUMBER*"|wc -l)
 	if [[ $NUMFILE -eq 0 ]]; then
-		echo "No files found in $ZIPFILE_DIR with wildcard opdracht_$FINDNUMBER*.zip" 
+		echo "No files found with wildcard $FINDNUMBER*" 
 	else
-		FILES=$(find $ZIPFILE_DIR -name "$FINDNUMBER*" -print);
-		for file in $FILES; do
-			echo $file;
-			#execute_command "$file" "$LOGLEVEL"
+		DIRECTORIES=$(find $FILE_DIR -name "$FINDNUMBER*" -print);
+		for directory in $DIRECTORIES; do
+			if [ -d "$directory" ]; then
+				 execute_single_directory $directory;
+			fi
 		done
 	fi
 else
-	echo "wait";
+	#do all directories
+	find_directory "GEO";
+	find_directory "LVBB_";
+	find_directory "OZON";
+	find_directory "TPOD";
 fi
-echo "</result>" >> result.xml
+echo "</result>">>$resultfile
