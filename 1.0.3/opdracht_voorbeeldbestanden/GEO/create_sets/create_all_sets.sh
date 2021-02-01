1cf1b8c37598131c54515c452b771c8319f6cfe9
BertVerheesGeonovum <b.verhees@geonovum.nl>
2021-02-01 08:53:30 +0000

    renaming scripts

---
+++ 1.0.3/opdracht_voorbeeldbestanden/GEO/create_sets/create_all_sets.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/sh
+
+# Test alle tekst_nummer directories
+
+cd ..
+for d in *; do
+    if [ -d "$d" ]; then
+	  create_sets/create_one_set.sh $d;
+	fi
+done
+rm ../opdrachten_gereed/opdracht_create_sets*.zip
---
+++ 1.0.3/opdracht_voorbeeldbestanden/GEO/create_sets/create_one_set.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/sh
+
+echo $1
+cd $1;
+RANDOM=tr -c -d '[:alnum:]' < /dev/urandom  | dd bs=4 count=8 2>/dev/null
+FILENAMEPART=$1_$RANDOM
+NEWLINE="<idLevering>id-publicatie-$FILENAMEPART</idLevering>";
+sed -i "s|.*idLevering.*|$NEWLINE|" opdracht.xml
+rm ../../opdrachten_gereed/opdracht_$1*.zip;
+zip a ../../opdrachten_gereed/opdracht_$FILENAMEPART.zip *;
+git add *;
+git add ../../opdrachten_gereed/opdracht_$FILENAMEPART.zip;
+git commit -a -m $1;git push;
+cd ..;
