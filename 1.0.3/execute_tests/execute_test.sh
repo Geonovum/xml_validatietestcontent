c90cffcd8afdeb31df53635134a62a8e370a2e02
BertVerhees <bert.verhees@rosa.nl>
2021-02-01 14:03:38 +0100

    improving scripts

---
+++ 1.0.3/execute_tests/execute_test.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+
+ZIPFILE_DIR="../opdracht_voorbeeldbestanden/opdrachten_gereed";
+FINDNUMBER=$1
+DEBUG=$2
+LOGLEVEL=""
+
+
+echo "<result>" > result.xml
+if [[ -n $FINDNUMBER ]]; then
+	echo "Processing with argument $FINDNUMBER";
+	#find file and test if is a directory
+	NUMFILE=$(find $ZIPFILE_DIR -name "opdracht_$FINDNUMBER*.zip"|wc -l)
+	if [[ $NUMFILE -eq 0 ]]; then
+		echo "No files found in $ZIPFILE_DIR with wildcard opdracht_$FINDNUMBER*.zip" 
+	else
+		FILES=$(find $ZIPFILE_DIR -name "opdracht_$FINDNUMBER*.zip" -print);
+		for file in $FILES; do
+			execute_command "$file" "$LOGLEVEL"
+		done
+	fi
+else
+fi
+echo "</result>" >> result.xml
