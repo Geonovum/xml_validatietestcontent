e3d197679d4e29fafe3e374dc4370394a8e2b853
BertVerheesGeonovum <b.verhees@geonovum.nl>
2021-02-02 10:09:11 +0000

    improving scripts

---
+++ 1.0.3/execute_tests/execute_without_rebuild.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+
+FILE_DIR="../opdracht_voorbeeldbestanden/opdrachten_gereed";
+FINDNUMBER=$1
+DEBUG=$2
+LOGLEVEL=""
+
+file=$1;	
+filename=${file##*/};
+echo $filename;
+filenamewithoutextension=${filename%.zip}
+echo $filenamewithoutextension
+conversationid=${filenamewithoutextension#*_}
+echo $conversationid
+result=$(oow-corv $log_level --action versturen --levering_id "id-publicatie-$conversationid" --conversation_id "$conversationid" --oin 00000001812579446000 --opdracht valideren "$file")
+#wait 10 seconds for keten to create results
+sleep 10
+#get result
+wget -nv --no-check-certificate $result -O result.xml;
