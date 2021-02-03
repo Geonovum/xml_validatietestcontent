ffea72b30da75a5f0b8db3ed6fb5e98d936d2eeb
BertVerhees <bert.verhees@rosa.nl>
2021-02-03 13:46:43 +0100

    LVBB1506

--- 1.0.3/opdracht_voorbeeldbestanden/create_sets/create_all_sets.sh
+++ 1.0.3/opdracht_voorbeeldbestanden/create_sets/create_all_sets.sh
@@ -2,9 +2,28 @@
 
 # Test alle tekst_nummer directories
 
-
+cd GEO
 for d in *; do
     if [ -d "$d" ]; then
 	  ../create_sets/create_one_set.sh $d;
 	fi
 done
+cd ../LVBB_
+for d in *; do
+    if [ -d "$d" ]; then
+	  ../create_sets/create_one_set.sh $d;
+	fi
+done
+cd ../OZON
+for d in *; do
+    if [ -d "$d" ]; then
+	  ../create_sets/create_one_set.sh $d;
+	fi
+done
+cd ../TPOD
+for d in *; do
+    if [ -d "$d" ]; then
+	  ../create_sets/create_one_set.sh $d;
+	fi
+done
+cd ..
