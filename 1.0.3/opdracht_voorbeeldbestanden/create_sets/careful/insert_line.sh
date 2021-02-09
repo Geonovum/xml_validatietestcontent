827d6cd54606cb626107a268a8d0384ac5f5b852
BertVerhees <bert.verhees@rosa.nl>
2021-02-09 13:41:27 +0100

    regelingsgebied.xml

--- 1.0.3/opdracht_voorbeeldbestanden/create_sets/careful/insert_line.sh
+++ 1.0.3/opdracht_voorbeeldbestanden/create_sets/careful/insert_line.sh
@@ -2,11 +2,5 @@
 
 # Test alle tekst_nummer directories
 
-		NEWLINE=" <bestand>\n     <bestandsnaam>owLocatieRegelingsgebied.xml</bestandsnaam>\n     <contentType>application/xml</contentType>\n    </bestand>";
-		sed -i "/.*manifest xmlns=\"http:\/\/www.overheid.nl\/2017\/lvbb\">.*/ a $NEWLINE" manifest.xml
-		NEWLINE=" <bestand>\n     <bestandsnaam>regelingsgebied.xml</bestandsnaam>\n     <contentType>application/xml</contentType>\n    </bestand>";
-		sed -i "/.*manifest xmlns=\"http:\/\/www.overheid.nl\/2017\/lvbb\">.*/ a $NEWLINE" manifest.xml
-		NEWLINE=" <bestand>\n     <bestandsnaam>regelingsgebied.gml</bestandsnaam>\n     <contentType>application/gml+xml</contentType>\n    </bestand>";
-		sed -i "/.*manifest xmlns=\"http:\/\/www.overheid.nl\/2017\/lvbb\">.*/ a $NEWLINE" manifest.xml
-		NEWLINE="  <Bestand>\n     <naam>owLocatieRegelingsgebied.xml</naam>\n      <objecttype>Gebied</objecttype>\n   </Bestand>";
-		sed -i "/.*DoelID>\/join\/id\/proces\/gm0297\/2019.*/ a $NEWLINE" manifest-ow.xml
+		NEWLINE="<formaatInformatieobject>/join/id/stop/informatieobject/gio_002</formaatInformatieobject>";
+		sed -i "/.*formaatInformatieobject>/join/id/stop/informatieobject.*/ a $NEWLINE" regelingsgebied.xml
--- 1.0.3/opdracht_voorbeeldbestanden/create_sets/careful/insert_lines.sh
+++ 1.0.3/opdracht_voorbeeldbestanden/create_sets/careful/insert_lines.sh
@@ -7,53 +7,29 @@ if [[ -d GEO ]]; then
 cd GEO
 for d in *; do
     if [ -d "$d" ]; then
-		NEWLINE=" <bestand>\n     <bestandsnaam>owLocatieRegelingsgebied.xml</bestandsnaam>\n     <contentType>application/xml</contentType>\n    </bestand>";
-		sed -i "/.*manifest xmlns=\"http:\/\/www.overheid.nl\/2017\/lvbb\">.*/ a $NEWLINE" $d/manifest.xml
-		NEWLINE=" <bestand>\n     <bestandsnaam>regelingsgebied.xml</bestandsnaam>\n     <contentType>application/xml</contentType>\n    </bestand>";
-		sed -i "/.*manifest xmlns=\"http:\/\/www.overheid.nl\/2017\/lvbb\">.*/ a $NEWLINE" $d/manifest.xml
-		NEWLINE="\n <bestand>\n     <bestandsnaam>regelingsgebied.gml</bestandsnaam>\n     <contentType>application/gml+xml</contentType>\n    </bestand>";
-		sed -i "/.*manifest xmlns=\"http:\/\/www.overheid.nl\/2017\/lvbb\">.*/ a $NEWLINE" $d/manifest.xml
-		NEWLINE="  <Bestand>\n     <naam>owLocatieRegelingsgebied.xml</naam>\n      <objecttype>Gebied</objecttype>\n   </Bestand>";
-		sed -i "/.*DoelID>\/join\/id\/proces\/gm0297\/2019.*/ a $NEWLINE" $d/manifest-ow.xml
+		NEWLINE="<formaatInformatieobject>/join/id/stop/informatieobject/gio_002</formaatInformatieobject>";
+		sed -i "s|.*formaatInformatieobject>/join/id/stop/informatieobject.*|$NEWLINE|" $d/regelingsgebied.xml
 	fi
 done
 cd ../LVBB_
 for d in *; do
     if [ -d "$d" ]; then
-		NEWLINE=" <bestand>\n     <bestandsnaam>owLocatieRegelingsgebied.xml</bestandsnaam>\n     <contentType>application/xml</contentType>\n    </bestand>";
-		sed -i "/.*manifest xmlns=\"http:\/\/www.overheid.nl\/2017\/lvbb\">.*/ a $NEWLINE" $d/manifest.xml
-		NEWLINE=" <bestand>\n     <bestandsnaam>regelingsgebied.xml</bestandsnaam>\n     <contentType>application/xml</contentType>\n    </bestand>";
-		sed -i "/.*manifest xmlns=\"http:\/\/www.overheid.nl\/2017\/lvbb\">.*/ a $NEWLINE" $d/manifest.xml
-		NEWLINE="\n <bestand>\n     <bestandsnaam>regelingsgebied.gml</bestandsnaam>\n     <contentType>application/gml+xml</contentType>\n    </bestand>";
-		sed -i "/.*manifest xmlns=\"http:\/\/www.overheid.nl\/2017\/lvbb\">.*/ a $NEWLINE" $d/manifest.xml
-		NEWLINE="  <Bestand>\n     <naam>owLocatieRegelingsgebied.xml</naam>\n      <objecttype>Gebied</objecttype>\n   </Bestand>";
-		sed -i "/.*DoelID>\/join\/id\/proces\/gm0297\/2019.*/ a $NEWLINE" $d/manifest-ow.xml
+		NEWLINE="<formaatInformatieobject>/join/id/stop/informatieobject/gio_002</formaatInformatieobject>";
+		sed -i "s|.*formaatInformatieobject>/join/id/stop/informatieobject.*|$NEWLINE|" $d/regelingsgebied.xml
 	fi
 done
 cd ../OZON
 for d in *; do
     if [ -d "$d" ]; then
-		NEWLINE=" <bestand>\n     <bestandsnaam>owLocatieRegelingsgebied.xml</bestandsnaam>\n     <contentType>application/xml</contentType>\n    </bestand>";
-		sed -i "/.*manifest xmlns=\"http:\/\/www.overheid.nl\/2017\/lvbb\">.*/ a $NEWLINE" $d/manifest.xml
-		NEWLINE=" <bestand>\n     <bestandsnaam>regelingsgebied.xml</bestandsnaam>\n     <contentType>application/xml</contentType>\n    </bestand>";
-		sed -i "/.*manifest xmlns=\"http:\/\/www.overheid.nl\/2017\/lvbb\">.*/ a $NEWLINE" $d/manifest.xml
-		NEWLINE="\n <bestand>\n     <bestandsnaam>regelingsgebied.gml</bestandsnaam>\n     <contentType>application/gml+xml</contentType>\n    </bestand>";
-		sed -i "/.*manifest xmlns=\"http:\/\/www.overheid.nl\/2017\/lvbb\">.*/ a $NEWLINE" $d/manifest.xml
-		NEWLINE="  <Bestand>\n     <naam>owLocatieRegelingsgebied.xml</naam>\n      <objecttype>Gebied</objecttype>\n   </Bestand>";
-		sed -i "/.*DoelID>\/join\/id\/proces\/gm0297\/2019.*/ a $NEWLINE" $d/manifest-ow.xml
+		NEWLINE="<formaatInformatieobject>/join/id/stop/informatieobject/gio_002</formaatInformatieobject>";
+		sed -i "s|.*formaatInformatieobject>/join/id/stop/informatieobject.*|$NEWLINE|" $d/regelingsgebied.xml
 	fi
 done
 cd ../TPOD
 for d in *; do
     if [ -d "$d" ]; then
-		NEWLINE=" <bestand>\n     <bestandsnaam>owLocatieRegelingsgebied.xml</bestandsnaam>\n     <contentType>application/xml</contentType>\n    </bestand>";
-		sed -i "/.*manifest xmlns=\"http:\/\/www.overheid.nl\/2017\/lvbb\">.*/ a $NEWLINE" $d/manifest.xml
-		NEWLINE=" <bestand>\n     <bestandsnaam>regelingsgebied.xml</bestandsnaam>\n     <contentType>application/xml</contentType>\n    </bestand>";
-		sed -i "/.*manifest xmlns=\"http:\/\/www.overheid.nl\/2017\/lvbb\">.*/ a $NEWLINE" $d/manifest.xml
-		NEWLINE="\n <bestand>\n     <bestandsnaam>regelingsgebied.gml</bestandsnaam>\n     <contentType>application/gml+xml</contentType>\n    </bestand>";
-		sed -i "/.*manifest xmlns=\"http:\/\/www.overheid.nl\/2017\/lvbb\">.*/ a $NEWLINE" $d/manifest.xml
-		NEWLINE="  <Bestand>\n     <naam>owLocatieRegelingsgebied.xml</naam>\n      <objecttype>Gebied</objecttype>\n   </Bestand>";
-		sed -i "/.*DoelID>\/join\/id\/proces\/gm0297\/2019.*/ a $NEWLINE" $d/manifest-ow.xml
+		NEWLINE="<formaatInformatieobject>/join/id/stop/informatieobject/gio_002</formaatInformatieobject>";
+		sed -i "s|.*formaatInformatieobject>/join/id/stop/informatieobject.*|$NEWLINE|" $d/regelingsgebied.xml
 	fi
 done
 cd ..
