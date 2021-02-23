a10499f30b2e0f7c260bfbea9cbc19c569bd9622
BertVerheesGeonovum <b.verhees@geonovum.nl>
2021-02-23 11:00:30 +0000

    GEO0001

--- 1.0.4/execute_tests/execute_all_without_rebuild.sh
+++ 1.0.4/execute_tests/execute_all_without_rebuild.sh
@@ -16,7 +16,7 @@ execute_single_file () {
 	conversationid=${filenamewithoutextension#*_}
 	result=$(oow-corv $log_level --action versturen --levering_id "id-publicatie-$conversationid" --conversation_id "$conversationid" --oin 00000001812579446000 --opdracht valideren "$file")
 	#wait ?? seconds for keten to create results
-	sleep 30
+	sleep 60
 	#get result
 	wget -nv --no-check-certificate $result -O result;
 	echo "<envelop>">>$resultfile
