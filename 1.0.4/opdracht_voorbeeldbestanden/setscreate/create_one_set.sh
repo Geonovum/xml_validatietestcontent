84b8c0caca2d345bee39ef6a42704eec86d57f34
BertVerheesGeonovum <bert.verhees@rosa.nl>
2021-02-24 15:23:12 +0100

    BHKV1004

--- 1.0.4/opdracht_voorbeeldbestanden/setscreate/create_one_set.sh
+++ 1.0.4/opdracht_voorbeeldbestanden/setscreate/create_one_set.sh
@@ -112,6 +112,9 @@ if [ -d "$1" ]; then
 		echo "changing heeftGeboorteregeling to <heeftGeboorteregeling>/akn/nl/act/$GEMEENTE/2019/$NEWWORD</heeftGeboorteregeling> in $giofile" 
 		sed -i "s|.*heeftGeboorteregeling>/akn/nl/act/$GEMEENTE/2019/.*|<heeftGeboorteregeling>/akn/nl/act/$GEMEENTE/2019/$NEWWORD</heeftGeboorteregeling>|" $giofile
 	done
+	echo "changing <WorkIDRegeling> to $NEWWORD in manifest-ow.xml" 
+	sed -i "s|.*WorkIDRegeling>.*|<WorkIDRegeling>/akn/nl/act/$GEMEENTE/2019/$NEWWORD</WorkIDRegeling>|" manifest-ow.xml
+
 	
 	#replacing hash in io
 	FILES=$(find . -name "*.gml" -print);
