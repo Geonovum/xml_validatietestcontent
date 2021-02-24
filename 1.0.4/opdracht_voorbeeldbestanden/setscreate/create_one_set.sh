de33dd17fd1926362158b1009ca383e8568e14ae
BertVerheesGeonovum <b.verhees@geonovum.nl>
2021-02-24 14:31:51 +0000

    BHKV1004

--- 1.0.4/opdracht_voorbeeldbestanden/setscreate/create_one_set.sh
+++ 1.0.4/opdracht_voorbeeldbestanden/setscreate/create_one_set.sh
@@ -113,7 +113,7 @@ if [ -d "$1" ]; then
 		sed -i "s|.*heeftGeboorteregeling>/akn/nl/act/$GEMEENTE/2019/.*|<heeftGeboorteregeling>/akn/nl/act/$GEMEENTE/2019/$NEWWORD</heeftGeboorteregeling>|" $giofile
 	done
 	echo "changing <WorkIDRegeling> to <WorkIDRegeling>/akn/nl/act/$GEMEENTE/2019/$NEWWORD</WorkIDRegeling> in manifest-ow.xml" 
-	sed -i "s|.*WorkIDRegeling>.*|<WorkIDRegeling>/akn/nl/act/$GEMEENTE/2019/$NEWWORD</WorkIDRegeling>|" manifest-ow.xml
+#	sed -i "s|.*WorkIDRegeling>.*|<WorkIDRegeling>/akn/nl/act/$GEMEENTE/2019/$NEWWORD</WorkIDRegeling>|" manifest-ow.xml
 
 	
 	#replacing hash in io
