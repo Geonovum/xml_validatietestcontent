02e476131ba5efd8d96d7f9b6c058ad67b871e8b
BertVerheesGeonovum <bert.verhees@rosa.nl>
2021-03-03 12:48:10 +0100

    LVBB4200

--- 1.0.4/opdracht_voorbeeldbestanden/setscreate/create_one_set.sh
+++ 1.0.4/opdracht_voorbeeldbestanden/setscreate/create_one_set.sh
@@ -31,7 +31,7 @@ if [ -d "$1" ]; then
 	#changing leveringid in ow bestanden
 	echo "changing levering_id to $NEWLINE6 in owRegelingsgebied.xml" 
 	sed -i "s|.*leveringsId.*|$NEWLINE6|" owRegelingsgebied.xml
-	if test "owLocaties-Gemeentestad.xml"; then
+	if [ -f "owLocaties-Gemeentestad.xml"]; then
 	   echo "changing levering_id to $NEWLINE6 in owLocaties-Gemeentestad.xml" 
 	   sed -i "s|.*leveringsId.*|$NEWLINE6|" owLocaties-Gemeentestad.xml
 	fi
