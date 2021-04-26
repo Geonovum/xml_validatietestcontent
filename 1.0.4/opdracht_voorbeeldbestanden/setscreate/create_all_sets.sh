a0e1be5efdbe7c64e62836225bd9bdc8891c59d1
BertVerheesGeonovum <bert.verhees@rosa.nl>
2021-04-26 12:16:29 +0200

    BHKV1003

--- 1.0.4/opdracht_voorbeeldbestanden/setscreate/create_all_sets.sh
+++ 1.0.4/opdracht_voorbeeldbestanden/setscreate/create_all_sets.sh
@@ -4,62 +4,64 @@
 
 if [[ -d GEO ]]; then
 
-cd BHKV
-for d in *; do
-   echo "Test: $d"
-   cmd_output=$(../setscreate/unique-one.sh $d)
-   if [[ $? -ne 0 ]] ; then 
-       exit 1 
-   fi
-done
+    cd BHKV
+    for d in *; do
+        echo "Test: $d"
+        cmd_output=$(../setscreate/unique-one.sh $d)
+        if [[ $? -ne 0 ]] ; then 
+            exit 1 
+        fi
+    done
 
-cd ../GEO
-for d in *; do
-   echo "Test: $d"
-   cmd_output=$(../setscreate/unique-one.sh $d)
-   if [[ $? -ne 0 ]] ; then 
-       exit 1 
-   fi
-done
+    cd ../GEO
+    for d in *; do
+        echo "Test: $d"
+        cmd_output=$(../setscreate/unique-one.sh $d)
+        if [[ $? -ne 0 ]] ; then 
+            exit 1 
+        fi
+    done
 
-cd ../LVBB_
-for d in *; do
-   echo "Test: $d"
-   cmd_output=$(../setscreate/unique-one.sh $d)
-   if [[ $? -ne 0 ]] ; then 
-       exit 1 
-   fi
-done
+    cd ../LVBB_
+    for d in *; do
+        echo "Test: $d"
+        cmd_output=$(../setscreate/unique-one.sh $d)
+        if [[ $? -ne 0 ]] ; then 
+            exit 1 
+        fi
+    done
 
-cd ../OZON
-for d in *; do
-   echo "Test: $d"
-   cmd_output=$(../setscreate/unique-one.sh $d)
-   if [[ $? -ne 0 ]] ; then 
-       exit 1 
-   fi
-done
+    cd ../OZON
+    for d in *; do
+        echo "Test: $d"
+        cmd_output=$(../setscreate/unique-one.sh $d)
+        if [[ $? -ne 0 ]] ; then 
+            exit 1 
+        fi
+    done
 
-cd ../STOP
-for d in *; do
-   echo "Test: $d"
-   cmd_output=$(../setscreate/unique-one.sh $d)
-   if [[ $? -ne 0 ]] ; then 
-       exit 1 
-   fi
-done
+    cd ../STOP
+    for d in *; do
+        echo "Test: $d"
+        cmd_output=$(../setscreate/unique-one.sh $d)
+        if [[ $? -ne 0 ]] ; then 
+            exit 1 
+        fi
+    done
 
-cd ../TPOD
-for d in *; do
-   echo "Test: $d"
-   cmd_output=$(../setscreate/unique-one.sh $d)
-   if [[ $? -ne 0 ]] ; then 
-       exit 1 
-   fi
-done
+    cd ../TPOD
+    for d in *; do
+        echo "Test: $d"
+        cmd_output=$(../setscreate/unique-one.sh $d)
+        if [[ $? -ne 0 ]] ; then 
+            exit 1 
+        fi
+    done
 
-cd ..
-rm opdrachten_gereed/opdracht_-.zip
+    cd ..
+    rm opdrachten_gereed/opdracht_-.zip
+    exit 0
 else
     echo "Please start from directory opdracht_voorbeeldbestanden."
+    exit 1
 fi
