352467798cb986059c652cb7c5ef3370c1d37ff9
BertVerheesGeonovum <bert.verhees@rosa.nl>
2021-06-21 14:30:51 +0200

    afbreek-test

--- 1.0.4-matrix-1.3-rc/execute_tests/execute_one_without_rebuild.sh
+++ 1.0.4-matrix-1.3-rc/execute_tests/execute_one_without_rebuild.sh
@@ -24,7 +24,7 @@ if [[ -e $1 ]]; then
     fi
     echo "De opdracht is: $action"
     result=$(oow-corv $log_level --action versturen --levering_id "id-publicatie-$conversationid" --conversation_id "$conversationid" --oin 00000001812579446000 --opdracht "$action" "$file")
-    echo "oow-corv $log_level --action versturen --levering_id "id-publicatie-$conversationid" --conversation_id "$conversationid" --oin 00000001812579446000 --opdracht "$action" "$file""
+#    echo "oow-corv $log_level --action versturen --levering_id "id-publicatie-$conversationid" --conversation_id "$conversationid" --oin 00000001812579446000 --opdracht "$action" "$file""
     	
 #wait ?? seconds for keten to create results
 
