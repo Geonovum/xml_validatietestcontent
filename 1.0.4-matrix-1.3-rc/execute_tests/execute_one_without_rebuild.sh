738684e5a4a5c68bc914f39d0feef24885ee1b4b
BertVerheesGeonovum <bert.verhees@rosa.nl>
2021-06-21 13:33:29 +0200

    afbreek-test

--- 1.0.4-matrix-1.3-rc/execute_tests/execute_one_without_rebuild.sh
+++ 1.0.4-matrix-1.3-rc/execute_tests/execute_one_without_rebuild.sh
@@ -9,40 +9,38 @@ LOGLEVEL=""
 resultfile=~/xml_validatietestcontent/results/result.xml
 
 if [[ -e $1 ]]; then
-
-file=$1;	
-filename=${file##*/};
-echo $filename;
-filenamewithoutextension=${filename%.zip}
-conversationid=${filenamewithoutextension#*_}
-echo $conversationid;
-action="valideren"
-if [[ $file == *-0.zip ]];then
-    action="publiceren"
-fi
-if [[ $file == *-afbreek.zip ]];then
-    action="afbreken"
-fi
-result=$(oow-corv $log_level --action versturen --levering_id "id-publicatie-$conversationid" --conversation_id "$conversationid" --oin 00000001812579446000 --opdracht "$action" "$file")
+    file=$1;	
+    filename=${file##*/};
+    echo $filename;
+    filenamewithoutextension=${filename%.zip}
+    conversationid=${filenamewithoutextension#*_}
+    echo $conversationid;
+    action="valideren"
+    if [[ $file == *-0.zip ]];then
+        action="publiceren"
+    fi
+    if [[ $file == *-afbreek.zip ]];then
+        action="afbreken"
+    fi
+    result=$(oow-corv $log_level --action versturen --levering_id "id-publicatie-$conversationid" --conversation_id "$conversationid" --oin 00000001812579446000 --opdracht "$action" "$file")
 	
-#result=$(oow-corv $log_level --action versturen --levering_id "id-publicatie-$conversationid" --conversation_id "$conversationid" --oin 00000001812579446000 --opdracht valideren "$file")
 #wait ?? seconds for keten to create results
 
-for i in {1..45}
-do
-    printf '.' > /dev/tty
-    sleep 1
-done
-echo ""
+    for i in {1..45}
+    do
+        printf '.' > /dev/tty
+        sleep 1
+    done
+    echo ""
 
 #get result
-echo " ">$resultfile
-wget -nv --no-check-certificate $result -O result;
-echo "<envelop>">$resultfile
-echo "<test>$conversationid</test>">>$resultfile
-cat result>>$resultfile;
-echo "</envelop>">>$resultfile
-rm result
+    echo " ">$resultfile
+    wget -nv --no-check-certificate $result -O result;
+    echo "<envelop>">$resultfile
+    echo "<test>$conversationid</test>">>$resultfile
+    cat result>>$resultfile;
+    echo "</envelop>">>$resultfile
+    rm result
 
 else
     echo "$1 is not found, please use an existing zip file"
